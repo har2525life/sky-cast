e609a0a82791784ed9f891cba498b6c0e4e71c92
har2525life <har2525life@outlook.com>
2023-09-05 09:53:34 +0900

    add: content page, ライブラリ

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import MapPage from "./page/map/MapPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ContentPage from "./page/content/ContentPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MapPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ContentPage />} />
+        <Route path="/map" element={<MapPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
